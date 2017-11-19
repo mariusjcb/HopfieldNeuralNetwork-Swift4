6e7591f75d0b7854cb37dce1c6b3373d95f7c719
Marius Ilie <marius.ilie@mready.net>
2017-11-19 21:49:47 +0200

    Initial Commit

---
+++ Hopfield Neural Network/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Hopfield Neural Network
+//
+//  Created by Marius Ilie on 19/11/2017.
+//  Copyright © 2017 Marius Ilie. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
